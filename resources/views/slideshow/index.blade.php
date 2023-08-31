624a62f0d2c37be0b7dcb1e1026e147999a69e9d
Muhammad Quraisy <iye.fredickson@gmail.com>
2023-08-31 14:57:50 +0700

    fix: design

--- resources/views/slideshow/index.blade.php
+++ resources/views/slideshow/index.blade.php
@@ -14,18 +14,18 @@
     <div class="relative w-screen h-screen bg-wisuda-28 bg-cover bg-no-repeat flex justify-center items-center ">
         @foreach ($graduates as $graduate)
             <div
-                class="absolute py-4 px-6 transition-opacity duration-700 flex flex-col {{ $loop->index == 0 ? 'current' : '' }} slide">
+                class="absolute py-4 px-6 mt-12 transition-opacity duration-700 flex flex-col {{ $loop->index == 0 ? 'current' : '' }} slide">
                 <div class="flex flex-row mb-4">
                     <div class="h-96 w-72 border-4 border-yellow-400 rounded-md mr-8 ">
-                        <img src="{{ asset('storage/images/28/' . $graduate->nim . '.jpg') }}" alt="Graduate Image"
+                        <img src="{{ asset('storage/images/28/' . $graduate->nim . '.jpg') }}" alt="{{$graduate->nim}}"
                             class="h-full w-full" loading="lazy">
                     </div>
 
                     <div
-                        class="flex min-w-[42rem] max-w-lg flex-col justify-center py-2 px-4 bg-white border-4 border-yellow-400 rounded-md ">
-                        <div class="font-bold text-2xl">{{ $graduate->nama }}</div>
+                        class="flex min-w-[48rem] max-w-lg flex-col justify-center py-2 px-4 bg-white border-4 border-yellow-400 rounded-md ">
+                        <div class="font-bold text-3xl">{{ $graduate->nama }}</div>
                         <div class="font-medium text-xl mb-2">{{ $graduate->nim }}</div>
-                        <div class="font-medium mb-2 text-lg">
+                        <div class="font-medium mb-2 text-xl">
                             {{ $graduate->department->id != 36 ? 'Judul Skripsi : ' . $graduate->judulSkripsi : 'Judul Tesis : ' . $graduate->judulSkripsi }}
                         </div>
                         <ul class="list-disc list-outside px-4">
